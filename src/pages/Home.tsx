a1762a9ff0b7c9dcfb36370a6f420ba89d2f7cac
Emie-Trialer <emiedonmukumo@gmail.com>
2025-07-18 16:23:50 +0100

    updated src/pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
-import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
+    const { isLoggedIn } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isLoggedIn) {
+            navigate('/dashboard');
+        }
+    }, [isLoggedIn]);
+
     return (
-        <main className="">
+        <main className="flex flex-col justify-center items-center min-h-screen">
             <h1 className="text-xl font-bold">Workcity Assessment (Frontend)</h1>
             <div className="mt-2 flex space-x-5">
                 <Link to={'#'} className="bg-gray-200 p-2 px-5 rounded-md hover:bg-gray-300 cursor-pointer">Signup</Link>
